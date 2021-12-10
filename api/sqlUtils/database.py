2c00278e983067a0ccfc7b8db18ee49bf3aa2cad
DanielFernández <n231bb@gmail.com>
2021-12-10 08:23:41 -0800

    Fix error reading from `.env`

--- api/sqlUtils/database.py
+++ api/sqlUtils/database.py
@@ -1,8 +1,10 @@
 import os
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 # Configure for cloud db
 SQL_DATABASE_URI = os.getenv('DB_URI')
